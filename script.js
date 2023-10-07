050fadc7862c60bfa405dcffadf08f4118cd4d2f
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-07 17:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	 if (celsius < -273.15 || celsius > 1.8e9) {
+        return "Invalid input"; // Temperature out of range
+    }
+
+    // Convert Celsius to Fahrenheit using the formula: (Celsius * 9/5) + 32
+    const fahrenheit = (celsius * 9/5) + 32;
+
+    // Round the result to two decimal places
+    return fahrenheit.toFixed(2);
 }
 
 // Do not change the code below
